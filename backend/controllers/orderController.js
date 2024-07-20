0a7c916e3a7def495f11d1b16b97071018c308cb
adohong4 <adohong4@gmail.com>
2024-07-20 23:52:59 +0700

    create user order page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -68,5 +68,16 @@ const verifyOrder = async (req, res) => {
     }
 }
 
+//user orders for frontend
+const userOrder = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
 
-export { placeOrder, verifyOrder };
+
+export { placeOrder, verifyOrder, userOrder };
